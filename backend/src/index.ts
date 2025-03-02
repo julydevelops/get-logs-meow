25395de2cbc42eb8a95fbc33e8a05148d0fa6c30
Julio Guereca <guereca.julio@gmail.com>
2025-03-01 19:37:51 -0800

    Update backend to include cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import logsRouter from './routes/logs.router';
+import cors from 'cors';
 import { errorHandler } from './middlerware/errorHandler';
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
